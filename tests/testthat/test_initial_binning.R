da8723218ee908b6a8db6e067b2065860d6862cb
abaldenko <abaldenko@massmutual.com>
2017-03-29 18:21:42 -0400

    easier to read test output

--- tests/testthat/test_initial_binning.R
+++ tests/testthat/test_initial_binning.R
@@ -1,3 +1,4 @@
+context("Initial Binning")
 # create df for testing -------
 set.seed(1)
 n <- 1000
@@ -20,17 +21,22 @@ for(i in 1:(ncol(df1)-1)){
 df1_tbl <- testthat_tbl('df1')
 
 # basic char tests -----------
-expect_is(bin_init_char(df1_tbl, 'b', 'z', .001), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'b', 'z', .01), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'b', 'z', .1), 'nominalbin')
+test_that("returning nomialbin?", {
+  expect_is(bin_init_char(df1_tbl, 'b', 'z', .001), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'b', 'z', .01), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'b', 'z', .1), 'nominalbin')
 
-expect_is(bin_init_char(df1_tbl, 'd', 'z', .001), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'd', 'z', .01), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'd', 'z', .1), 'nominalbin')
-
-expect_is(bin_init_char(df1_tbl, 'f', 'z', .001), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'f', 'z', .01), 'nominalbin')
-expect_is(bin_init_char(df1_tbl, 'f', 'z', .1), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'd', 'z', .001), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'd', 'z', .01), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'd', 'z', .1), 'nominalbin')
+  
+  
+  expect_is(bin_init_char(df1_tbl, 'f', 'z', .001), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'f', 'z', .01), 'nominalbin')
+  expect_is(bin_init_char(df1_tbl, 'f', 'z', .1), 'nominalbin')
+})
 
 # not working yet...
-# expect_is(bin_init_num(df1_tbl, 'c', 'z', 5), 'intervalbin')
+# test_that("returning intervalbin?", {
+#   expect_is(bin_init_num(df1_tbl, 'c', 'z', 5), 'intervalbin')
+# })
